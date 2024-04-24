7b9de7f338962c15d38f0a0ddef4f9305b3a6762
SebastianCavalcante <sebastiancavalcante073@outlook.com>
2024-04-24 16:05:27 -0300

    Tratamento de Exceções, Blocos try, catch e finally

--- TratamentoDeExcecoes/Program.cs
+++ TratamentoDeExcecoes/Program.cs
@@ -2,7 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        try
+        Console.Write("Digite o texto que ira exibir no bloco finally: ");
+        string text = Console.ReadLine();
+
+        try   // Bloco try tenta executar o código
         {
             int n1 = int.Parse(Console.ReadLine());
             int n2 = int.Parse(Console.ReadLine());
@@ -11,18 +14,22 @@
 
             Console.WriteLine(div);
         }
-        
-        catch (FormatException e) 
+
+        catch (FormatException e)  // Lança uma exceção informando que o valor inserido não é um número.
         {
             Console.WriteLine("Valor inserido não é um número");
         }
-        catch (DivideByZeroException e)
+        catch (DivideByZeroException e) // Lança uma exceção informando que não pode ser feita a divisão por zero.
         {
             Console.WriteLine("Não é possivel dividir por 0 ");
         }
-        catch (OverflowException e)
+        catch (OverflowException e) // Lança uma exceção informando que foi excedido o valor máximo permitido pelo tipo de dados.
         {
             Console.WriteLine("Valor inserido maior que o suportado pelo tipo " + e.Message);
+        }
+        finally  // Executa o bloco finally quaisquer que seja o resultado do programa.
+        {
+            Console.WriteLine("Executando o bloco finally: " + text);
         }
     }
 }
