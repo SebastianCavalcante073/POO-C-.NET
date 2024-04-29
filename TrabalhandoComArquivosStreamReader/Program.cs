029dfa158bd3b8761fd48d65c9aa0759855daf2c
SebastianCavalcante <sebastiancavalcante073@outlook.com>
2024-04-29 08:10:27 -0300

    StreamReader, Trabalhando com arquivos

---
+++ TrabalhandoComArquivosStreamReader/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = @"c:\temp\file1.txt"; // Variável path recebe o directorio do arquivo file1.txt na pasta temp
+
+        StreamReader sr = null; // Define como null.
+
+        try
+        {
+            sr = File.OpenText(path); // Instância o StreamReader e abre o arquivo.
+            while (!sr.EndOfStream) // Faz a leitura de todas as linhas do arquivo.
+            {
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+        } catch (IOException e) 
+        {
+            Console.WriteLine("Ocorreu um erro!");
+            Console.WriteLine(e.Message);
+        }
+        finally // Vefica se o arquivo sr está aberto "!null" e então o fecha!
+        {
+            if (sr != null) sr.Close(); 
+        }
+    }
+}
